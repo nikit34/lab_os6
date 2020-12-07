f519ba5ea3deae4082753cdd0d0dd0cd5586c727
Никита Пермяков <permikov134@mail.ru>
2020-12-07 16:18:09 +0500

    add define methods of tree

--- struct_server.cpp
+++ struct_server.cpp
@@ -9,10 +9,74 @@ class BinTree {
             this->delete_recursive(this->head);
         }
 
-        std::vector<int> get_all_nodes() const {
+        std::vector<int> get_all_nodes() {
             std::vector<int> result;
             this->all_nodes(this->head, result);
             return result;
+        }
+
+        bool contains(int& id) const{
+            Node* tmp = this->head;
+            while(tmp != nullptr){
+                if(tmp->id == id)
+                    break;
+                else if(id > tmp->id)
+                    tmp = tmp->right;
+                else if(id < tmp->id)
+                    tmp = tmp->right;
+            }
+            return tmp != nullptr;
+        }
+
+        void insert(int& id){
+            if(this->head == nullptr){
+                this->head = new Node(id);
+                return;
+            }
+            Node* tmp = this->head;
+            while(tmp != nullptr){
+                if(tmp->id == id)
+                    return;
+                else if(id < tmp->id){
+                    if(tmp->left == nullptr){
+                        tmp->left = new Node(id);
+                        return;
+                    }
+                    tmp = tmp->left;
+                }
+                else if(id > tmp->id){
+                    if(tmp->right == nullptr){
+                        tmp->right = new Node(id);
+                        return;
+                    }
+                    tmp = tmp->right;
+                }
+            }
+        }
+
+        void erase(int& id){
+            Node* prev = nullptr;
+            Node* tmp = this->head;
+            while(tmp != nullptr){
+                if (id == tmp->id) {
+                    if (prev == nullptr) {
+                        this->head = nullptr;
+                    } else {
+                        if (prev->left == tmp) {
+                            prev->left = nullptr;
+                        } else {
+                            prev->right = nullptr;
+                        }
+                    }
+                    delete_recursive(tmp);
+                } else if(id < tmp->id) {
+                    prev = tmp;
+                    tmp = tmp->left;
+                } else if(id > tmp->id) {
+                    prev = tmp;
+                    tmp = tmp->right;
+                }
+            }
         }
 
     private:
