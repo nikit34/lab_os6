d3eef50763cb32cf8a20bc17e5e29ac97e31edf5
Никита Пермяков <permikov134@mail.ru>
2020-12-07 16:15:48 +0500

    add unit test for create node

--- tests/unittests.cpp
+++ tests/unittests.cpp
@@ -1,21 +1,51 @@
-#include"./struct_server.cpp"
+#include"./../struct_server.cpp"
 #include <iostream>
 #include <cassert>
 
 
 class TestBitTree{
     public:
-        void test_create_default_node(){
-            BinTree *t = new BinTree();
-            assert(t->head == nullptr && "[ERROR] create default fields of node");
-            assert(t->head->left == nullptr && "[ERROR] create default fields of node");
-            assert(t->head->right == nullptr && "[ERROR] create default fields of node");
+        TestBitTree(){
+            this->tree = new BinTree();
+
         }
+        static void test_create_default_node(){
+            int test_id_1 = 5;
+            BinTree* tree = new BinTree();
+            tree->head = new BinTree::Node(test_id_1);
+            assert(tree->head != nullptr && "[ERROR] create default head node");
+            assert(tree->head->id == test_id_1 && "[ERROR] create default head node");
+            assert(tree->head->left == nullptr && "[ERROR] create default left node");
+            assert(tree->head->right == nullptr && "[ERROR] create default right node");
+        }
+        // void test_insert(){
+        //     BinTree *t = new BinTree();
+        //     int test_id_1 = 5;
+        //     int test_id_2 = 3;
+        //     t->insert(test_id_1);
+        //     assert(t->head->id == test_id_1 && "[ERROR] insert test_id_1");
+        //     t->insert(test_id_2);
+        //     if(test_id_1 > test_id_2){
+        //         assert(t->head->id == test_id_1 && "[ERROR] insert test_id_2");
+        //         assert(t->head->left->id == test_id_2 && "[ERROR] insert test_id_2");
+        //     }
+        //     else if(test_id_1 < test_id_2){
+        //         assert(t->head->id == test_id_1 && "[ERROR] insert test_id_2");
+        //         assert(t->head->right->id == test_id_2 && "[ERROR] insert test_id_2");
+        //     } else {
+        //         assert(t->head->id == test_id_1 && "[ERROR] insert repeat");
+        //     }
+        // }
+        // void test_erase(){
+
+        // }
+    private:
+        BinTree* tree;
 };
 
 int main(){
-    TestBitTree tree;
-    tree.test_create_default_node();
-
+    TestBitTree::test_create_default_node();
+    // tree.test_insert();
+    // tree.test_erase();
     return 0;
 }
