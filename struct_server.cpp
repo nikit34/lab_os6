4351879596ec488e229acd006b54cf269023806e
Никита Пермяков <permikov134@mail.ru>
2020-12-07 13:56:54 +0500

    fix struct tree

--- struct_server.cpp
+++ struct_server.cpp
@@ -1,24 +1,45 @@
+#include<iostream>
+#include<vector>
+
+
 class BinTree {
     public:
         BinTree() = default;
         ~BinTree(){
-            delete_recursive(this->head);
+            this->delete_recursive(this->head);
+        }
+
+        std::vector<int> get_all_nodes() const {
+            std::vector<int> result;
+            this->all_nodes(this->head, result);
+            return result;
         }
 
     private:
-        void delete_recursive(BinTree* node){
-            if(this->head == nullptr){
+        struct Node {
+            Node(int id): id(id) {}
+            int id;
+            Node* left = nullptr;
+            Node* right = nullptr;
+        };
+
+        void all_nodes(Node* node, std::vector<int> vec) const{
+            if(node == nullptr)
                 return;
-            }
+            this->all_nodes(node->left, vec);
+            vec.push_back(node->id);
+            this->all_nodes(node->right, vec);
+        }
+
+        void delete_recursive(Node* node){
+            if(node == nullptr)
+                return;
             delete_recursive(node->right);
             delete_recursive(node->left);
             delete node;
         }
 
-        BinTree* head = nullptr;
-        int id;
-        BinTree* right = nullptr;
-        BinTree* left = nullptr;
+        Node* head = nullptr;
 
         friend class TestBitTree;
 };
--- tests/unittests.cpp
+++ tests/unittests.cpp
@@ -5,16 +5,17 @@
 
 class TestBitTree{
     public:
-        void test_create_default_nodes(){
+        void test_create_default_node(){
             BinTree *t = new BinTree();
             assert(t->head == nullptr && "[ERROR] create default fields of node");
-            assert(t->left == nullptr && "[ERROR] create default fields of node");
-            assert(t->right == nullptr && "[ERROR] create default fields of node");
+            assert(t->head->left == nullptr && "[ERROR] create default fields of node");
+            assert(t->head->right == nullptr && "[ERROR] create default fields of node");
         }
 };
 
 int main(){
     TestBitTree tree;
-    tree.test_create_default_nodes();
+    tree.test_create_default_node();
+
     return 0;
 }
